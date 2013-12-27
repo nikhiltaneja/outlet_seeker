76178067dfe31e8e721f1fb25791bfe02267cebd
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-12-27 17:02:26 -0500

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+CoffeeShop.delete_all
+
+c1 = CoffeeShop.create(
+  name: "Octane",
+  address: "1009 Marietta St NW",
+  city: "Atlanta",
+  state: "GA",
+  outlets: 10
+  )
+
+c2 = CoffeeShop.create(
+  name: "Dancing Goats",
+  address: "419 W Ponce De Leon Ave",
+  city: "Decatur",
+  state: "GA",
+  outlets: 6
+  )
+
+c3 = CoffeeShop.create(
+  name: "Inman Perk",
+  address: "240 N Highland Ave",
+  city: "Atlanta",
+  state: "GA",
+  outlets: 8
+  )
+
+
